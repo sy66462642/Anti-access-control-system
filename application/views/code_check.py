71dd1ae16b8797cf747cbc49d931c145fce86679
JM-98 <1207213363@qq.com>
2020-11-13 12:24:02 +0800

    添加codeCheck部分逻辑

--- application/views/code_check.py
+++ application/views/code_check.py
@@ -1,6 +1,6 @@
 
 from flask import *
-from ..model import students
+from ..model import students,code
 from sqlalchemy.orm import sessionmaker
 from ..DB import db
 
@@ -13,5 +13,22 @@ def check_code():
         print('get_arrive')
         return render_template('/codeCheck.html')
     else:
+        print('post_arrive')
+        id=request.form.get('id')
+        code=request.form.get('code')
+
+        user=code.query.filter(code.id==id).first()
+        user_code=user.code
+        status=user.status
+        if user_code == code:
+            code.query.filter_by(code.id==id).update('proof_num':'20')
+            return render_template('/')
+        else:
+            if status==0:
+                students.query.filter_by(students.id==id).update('status':'1')
+                return render_template('/inCampus.html')
+            else:
+                students.query.filter_by(students.id==id).update('status':'0')
+                return render_template('/outCampus.html')
 
     return render_template('/regist.html')

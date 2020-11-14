3948592dd9112a903b638580ae6ac05b98ff8abc
sy66462642 <965602600@qq.com>
2020-11-14 11:50:09 +0800

    解决命名冲突

--- application/views/login.py
+++ application/views/login.py
@@ -13,29 +13,34 @@ def login():
         return render_template('/login.html')
     else:
         print('post_arrive')
-        id = request.form.get('username')
+        Id = request.form.get('username')
         pwd = request.form.get('password')
-        print(id)
-        user = students.query.filter(id==students.id).first()
+        print(Id)
+        print(type(Id))
+        user = students.query.filter(Id==students.id).first()
         if user and (pwd == user.password):
             session['user_id'] = user.id
             status = user.status
-            user_proof = code.query.filter(id == user.id).first()
+            print(type(user.id))
+            print((code.query.filter(int(Id) == code.id)))
+            user_proof = code.query.filter(int(Id) == code.id).first()
             if not user_proof:
+                """
                 return render_template('/inCampus.html',user_name=user.name, user_age=user.age, user_gender=user.gender,
-                               user_department=user.department, user_photo=user.selfie)
+                               user_department=user.department, user_photo=user.selfie)"""
+                return render_template('/login.html')
             user_proof_num = user_proof.proof_num
             if user_proof_num == 0:
                 return render_template('/codeCheck.html')
             else:
-                code.query.filter(id == code.id).update({'proof_num', str(user_proof_num - 1)})
+                code.query.filter(Id == code.id).update({'proof_num', str(user_proof_num - 1)})
                 db.session.commit()
                 if status == 0:
-                    students.query.filter(students.id == id).update({'status': '1'})
+                    students.query.filter(students.id == Id).update({'status': '1'})
                     db.session.commit()
                     return render_template("/outCampus.html")
                 else:
-                    students.query.filter(students.id == id).update({'status': '0'})
+                    students.query.filter(students.id == Id).update({'status': '0'})
                     db.session.commit()
                     return render_template("/inCampus.html")
         else:
